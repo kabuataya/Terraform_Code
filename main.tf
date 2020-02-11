2b860bd7e9368e93ee2a35241dd7e66f6a3b664a
Karam@VMware3 <kabuataya@gmail.com>
2020-02-11 23:29:32 +0300

    using autoscaling grou[

--- main.tf
+++ main.tf
@@ -7,21 +7,58 @@ variable "Web_Port" {
   type = number
   default = 8080
 }
-resource "aws_instance" "example" {
-  ami           = "ami-0df89c0ad05708804"
-  instance_type = "t3.nano"
-  vpc_security_group_ids = [aws_security_group.Port_8080_Allow.id]
+variable "Server_Count" {
+  description = "number of servers to be deployed"
+  type = number
+  default = 2
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+resource "aws_launch_configuration" "Web_Servers" {
+  image_id        = "ami-0df89c0ad05708804"
+  instance_type   = "t3.nano"
+  security_groups = [aws_security_group.Port_8080_Allow.id]
+
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World. this is terraform learning by Karam" > index.html
-              nohup busybox httpd -f -p ${var.Web_Port} &
-               EOF
-  tags = {
-    Name = "terraform-example"
-    Type = "Small"
-    Create_By = "Kay"
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+  lifecycle {
+    create_before_destroy = true
+            }
+}
+resource "aws_autoscaling_group" "Web_Scale" {
+  launch_configuration = aws_launch_configuration.Web_Servers.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.ids
+  min_size = 2
+  max_size = 10
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
+#resource "aws_instance" "example" {
+#  ami           = "ami-0df89c0ad05708804"
+#  instance_type = "t3.nano"
+#  vpc_security_group_ids = [aws_security_group.Port_8080_Allow.id]
+#  user_data = <<-EOF
+#              #!/bin/bash
+#              echo "Hello, World. this is terraform learning by Karam" > index.html
+#              nohup busybox httpd -f -p ${var.Web_Port} &
+#               EOF
+#  tags = {
+#    Name = "terraform-example"
+#    Type = "Small"
+#    Create_By = "Kay"
+#  }
+#}
 resource "aws_security_group" "Port_8080_Allow" {
   name = "terraform-example-instance"
 
@@ -31,4 +68,8 @@ resource "aws_security_group" "Port_8080_Allow" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+output "public IP" {
+  value = aws_instance.example.public_ip
+  description ="the public ip of the server generated"
 }
