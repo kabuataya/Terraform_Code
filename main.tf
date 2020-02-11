c1340d648e377c703015e15e03855805e199cfbe
Karam@VMware3 <kabuataya@gmail.com>
2020-02-11 16:47:42 +0300

    changed security grou[

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0df89c0ad05708804"
   instance_type = "t3.nano"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.Port_8080_Allow.id]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
@@ -16,7 +16,7 @@ resource "aws_instance" "example" {
     Create_By = "Kay"
   }
 }
-resource "aws_security_group" "instance" {
+resource "aws_security_group" "Port_8080_Allow" {
   name = "terraform-example-instance"
 
   ingress {
