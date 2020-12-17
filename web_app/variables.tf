8a8101f318b318859a561443caeb36efb669dfc7
Karam@VMware3 <kabuataya@gmail.com>
2020-12-17 10:09:24 +0300

    apply new changes

--- web_app/variables.tf
+++ web_app/variables.tf
@@ -1,12 +1,18 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
 variable "server_port" {
   description = "ports the server will connect to"
   type = number
   default = 8080
 }
+
 variable "cluster_name" {
   description = "the name to use for the the cluster resources"
   type = string
 }
+
 variable "min_size" {
   description = "the minimum size of the EC2 Instances"
   type = number
@@ -16,8 +22,21 @@ variable "max_size" {
   description = "the maximum number of the EC2 insatnces"
   type = number
 }
+
 variable "instance_type" {
   description = "the type of EC2 instance"
+}
+
+variable "custom_tags" {
+  description = "Custom tags to set on the Instances in the ASG"
+  type        = map(string)
+  default     = {}
+}
+
+variable "enable_autoscaling" {
+  description = "if set to true, then autom scale will be anabled"
+  type = bool
+  default = false
 }
 locals {
   http_port = 80
@@ -25,14 +44,4 @@ locals {
   any_protocol = "-1"
   tcp_protocol = "tcp"
   all_ips = ["0.0.0.0/0"]
-}
-variable "custom_tags" {
-  description = "Custom tags to set on the Instances in the ASG"
-  type        = map(string)
-  default     = {}
-}
-variable "enable_autoscaling" {
-  description = "flag for auto scale"
-  type = bool
-  default = false
 }
