1048a7f9b5c48d19acf7b228742f0a9575e7127d
Karam@VMware3 <kabuataya@gmail.com>
2020-12-21 00:04:30 +0300

    new script for modefied HTML

--- web_app/user-data-modefied.sh
+++ web_app/user-data-modefied.sh
@@ -4,5 +4,6 @@ cat > index.html <<EOF
 <h1>Hello, World</h1>
 <h1>My Name is Karam, and i am learning terraform</h1>
 <h1>hopefully it works this time and i can finisht the book</h1>
+
 EOF
 nohup busybox httpd -f -p ${server_port} &
