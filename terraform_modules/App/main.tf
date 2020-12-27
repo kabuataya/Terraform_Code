0a44c70d2b9703741e33ff099df43deb357b36ac
Karam@VMware3 <kabuataya@gmail.com>
2020-12-27 18:13:51 +0300

    changed capital letters to small letters

--- terraform_modules/App/main.tf
+++ terraform_modules/App/main.tf
@@ -36,3 +36,43 @@ data "template_file" "user_data" {
     server_port = var.server_port
   }
 }
+
+resource "aws_lb_target_group" "asg" {
+  name = "${var.environment}-tg"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id  = data.aws_vpc.default.id
+  
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_alb_listener_rule" "asg" {
+  listener_arn = module.alb.alb_http_listener_arn
+  priority = 100
+
+  condition {
+    path_pattern {
+    values = ["*"] 
+    }
+  }
+
+  action {
+
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+data "aws_vpc" "default" {
+  default = true
+}
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
