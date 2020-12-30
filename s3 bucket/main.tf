55f506dbf77d0665d632496884282a919083c7dd
Karam@VMware3 <kabuataya@gmail.com>
2020-12-30 14:31:28 +0300

    changed s3 bucket code

--- s3 bucket/main.tf
+++ s3 bucket/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "us-east-2"
 }
-resource "aws_s3_bucket" "terraform_state" {
-    bucket = "${var.db_remote_Stack_bucket}-karam"
+resource "aws_s3_bucket" "AWS_Bucket" {
+    bucket = "${var.db_remote_Stack_bucket}-vRA"
    /*
     lifecycle {
       prevent_destroy = true
--- s3 bucket/variables.tf
+++ s3 bucket/variables.tf
@@ -1,17 +1,3 @@
-variable "server_port" {
-  description = "ports the server will connect to"
-  type = number
-  default = 8080
-}
-variable "cluster_name" {
-  description = "the name to us for the the cluster resources"
-  type = string
-}
-
-variable "db_remote_Stack_bucket" {
+variable "bucket_name" {
   description = "the name of the s3 bucket"
-}
-
-variable "db_remote_State_key" {
-  description = "the path for the database remote state in S3"
 }
